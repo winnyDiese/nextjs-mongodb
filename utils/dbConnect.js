600869727b4beb35f71dabe924b9e00e2f80a2a6
winnyDiese <winnydiese@gmail.com>
2022-09-07 18:05:16 +0200

    Successfully connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -4,43 +4,60 @@ import mongoose from 'mongoose'
 
 const MONGODB_URL = process.env.MONGODB_URL
 
-if(!MONGODB_URL){
-    throw new Error(
+// if(!MONGODB_URL){
+//     throw new Error(
 
-    )
-}
-
-
-let cached = global.mongoose
-if(!cached){
-    cached = global.mongoose = {
-        conn: null,
-        promise: null
-    }
-}
+//     )
+// }
 
 
-async function dbConnect(){
-    if(cached.conn){
-        return cached.conn
-    }
+// let cached = global.mongoose
+// if(!cached){
+//     cached = global.mongoose = {
+//         conn: null,
+//         promise: null
+//     }
+// }
 
-    if(!cached.promise){
-        const opts = {
-            bufferCommand: false
-        }
 
-        cached.promise = mongoose.connect(MONGODB_URL)
-        .then((mongoose)=>{
-            return mongoose 
-        })
+// async function dbConnect(){
+//     if(cached.conn){
+//         return cached.conn
+//     }
 
-    }
+//     if(!cached.promise){
+//         const opts = {
+//             bufferCommand: false
+//         }
+
+//         cached.promise = mongoose.connect(MONGODB_URL)
+//         .then((mongoose)=>{
+//             return mongoose 
+//         })
+
+//     }
     
 
-    cached.conn = await cached.promise
-    return cached.conn
+//     cached.conn = await cached.promise
+//     return cached.conn
 
+// }
+
+
+// export default dbConnect()
+
+
+
+
+const dbConnect = async ()=> {
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('Mongodb connection SUCCESSFULL ')
+    }catch(error){
+        console.error('Mongodb connection Fail')
+        // console.log(error)
+    }
 }
 
-export default dbConnect()
+module.exports = dbConnect
+
